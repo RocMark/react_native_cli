a772a66da441e3a89ef7ffeb8ae6d980f86f158c
RocMark <onigir77799@gmail.com>
2020-07-06 14:18:39 +0800

    :zap: ADD Style variables File

---
+++ src/styles/variables.js
@@ -0,0 +1,18 @@
+import { Dimensions } from 'react-native'
+
+const colors = {
+  primary: '',
+}
+
+const containerSize = {
+  fullHeight: Dimensions.get('window').height,
+  fullWidth: Dimensions.get('window').width,
+}
+
+const fontSize = {
+  sm: 12,
+  md: 18,
+  lg: 28,
+}
+
+export { colors, containerSize, fontSize }
